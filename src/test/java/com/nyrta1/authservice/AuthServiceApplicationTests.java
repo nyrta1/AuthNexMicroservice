0d499e0d4db6530a46d15271e49d1b1e5176f174
nyrta1 <nurtai_2005@mail.ru>
2024-05-10 14:57:49 +0500

    build: build project

---
+++ src/test/java/com/nyrta1/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyrta1.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
